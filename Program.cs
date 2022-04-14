5adb204da1a4aab6c7b7cc343968fff3c397efea
gregory <wiznergregory@gmail.com>
2022-04-14 19:27:45 -0400

    Exercize13

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿int userInt = 0;
+
+Console.WriteLine("Hello, User. Please enter a whole number:");
+string userInput = Console.ReadLine();
+int.TryParse(userInput, out userInt);
+for (int currInt = userInt; currInt >= 0; currInt--)
+{
+    Console.WriteLine(currInt);
+}
+Console.ReadLine();
